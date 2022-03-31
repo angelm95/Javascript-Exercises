5d3ad0034b23fe81fe016c5be29c03ccd39b34e9
angelm95 <58274148+angelm95@users.noreply.github.com>
2022-03-31 17:50:16 -0400

    Add files via upload

---
+++ doms-patterns.js
@@ -0,0 +1,25 @@
+
+const colorsArr = ['red', 'orange', 'yellow','pink','green', 'blue', 'indigo', 'violet'];
+let topPosition = 50;
+let leftPosition = 50;
+let width = 500;
+let height = 500;
+
+while (width > 50) {
+const randomColorIdx = Math.floor(Math.random() * colorsArr.length);
+const newDiv = document.createElement('div');
+
+newDiv.style.top = topPosition + 'px';
+newDiv.style.left = leftPosition + 'px';
+newDiv.style.width = width + 'px';
+newDiv.style.height = height + 'px';
+newDiv.style.background = colorsArr[randomColorIdx];
+
+document.body.appendChild(newDiv);
+
+topPosition += 10;
+leftPosition += 10;
+width -= 20;
+height -= 20;
+}
+
