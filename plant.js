775376b82fa9ecbd67b9442c583c57d3328cc067
angelm95 <58274148+angelm95@users.noreply.github.com>
2022-04-05 23:02:38 -0400

    Add files via upload

--- plant.js
+++ plant.js
@@ -1,10 +1,14 @@
 const light = document.querySelector('#plant');
-
 light.addEventListener('click', function(e){
-
+   
     e.target.classList.toggle('plant-4')
+    light.classList.add('playing')
+})
 
-
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
 }
+const transition = document.querySelectorAll('.plant-3');
+transition.forEach(light => light.addEventListener('transitionend',removeTransition));
 
-)
